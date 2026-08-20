a17cab9ddd07e8e5a3d8a131021ee73828303ff5
mmohsen <mmohsen547@gmail.com>
2026-08-21 02:07:31 +0300

    fix: fix security issues

--- json.go
+++ json.go
@@ -30,5 +30,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, writeErr := w.Write(dat)
+
+	if writeErr != nil {
+		log.Printf("failed to write response: %v", err)
+	}
+
 }
